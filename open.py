66695c21d27c2feb4c2523a435198cf631aa1da3
MuriloPetruci <41083140+MuriloPetruci@users.noreply.github.com>
2020-02-10 04:08:57 -0300

    Update open.py

--- open.py
+++ open.py
@@ -1,4 +1,6 @@
-#Os endereços P2SH são a codificação Base58 Check do hash160 de um script (conhecido como redimirScript). Ele usa um byte de versão 0x05. O restante da codificação é o mesmo, apenas a codificação Base58 Check.
+#Os endereços P2SH são a codificação Base58 Check do hash160 de um script (conhecido como redimirScript). 
+#Ele usa um byte de versão 0x05. 
+#O restante da codificação é o mesmo, apenas a codificação Base58 Check.
 
 def hash160(self, v):
         r = hashlib.new('ripemd160')
